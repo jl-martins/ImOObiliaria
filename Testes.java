e6bd214fd5f421529ccb851d00ab5bc3bda42fb1
Joao Pereira <joaopereira_19@hotmail.com>
2016-05-13 15:49:16 +0100

    já funcionam outra vez os testes

--- Testes.java
+++ Testes.java
@@ -1,5 +1,4 @@
 
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -29,48 +28,53 @@ public class Testes
         imo = new Imoobiliaria();
         try {
             imo.iniciaSessao("",null);
-			fail();
+            fail();
         } catch(SemAutorizacaoException e) {
-            
+
         } catch(Exception e) {
             fail();
         }
-        
+
         try {
             v = new Vendedor("a75273@uminho.pt", "João Pereira", "1234", "Somewhere, Over the Rainbow, Way Up High", LocalDate.of(1996, 12, 19));
             imo.registarUtilizador(v);
         } catch(Exception e) {
             fail();
         }
-        
+
         String email = v.getEmail();
         String password = v.getPassword();
-        
+
         try {
             imo.iniciaSessao(email, password);
         } catch(Exception e) {
             fail();
         }
-        
+
         t = new Terreno("Terreno para construçao de Habitacoes #1", "Rua Sobe e Desce", 40000, 30000, 500, true,
-                   false, 20.0, 140.0, true);  // Preencher parâmetros do construtor
+            false, 20.0, 140.0, true);  // Preencher parâmetros do construtor
         try {
             imo.registaImovel(t);
         } catch (Exception e) {
             fail();
         }
-            
+
         int s = imo.getImovel("Terreno", Integer.MAX_VALUE).size();
         assertTrue(s>0);
-        Set<String> ids = imo.getTopImoveis(0);
-        assertTrue(ids.contains(t.getId()));
+        try{
+            Set<String> ids = imo.getTopImoveis(0);
+            assertTrue(ids.contains(t.getId()));
+        } catch(Exception e) {  
+            fail();
+        }
+
         assertTrue(imo.getMapeamentoImoveis().keySet().contains(t));
         try {
             assertTrue(imo.getConsultas().size()>0);
         } catch(Exception e) {
             fail();
         }
-        
+
         imo.fechaSessao();
         Comprador c = new Comprador("a68646@uminho.pt", "João Martins", "1234", "Jardim Zoológico da Maia", LocalDate.of(1994, 8, 8));  // Preencher parâmetros do construtor
         try {
@@ -89,5 +93,5 @@ public class Testes
             fail();
         }
     }
-    
+
 }
